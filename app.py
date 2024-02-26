6c5643277defc94450a425a80a4053964bd23b43
krishantt <154585072+krishantt@users.noreply.github.com>
2024-02-26 16:29:48 +0545

    graph

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
 import streamlit as st
 
 st.title("Start")
+st.graphviz_chart('''
+    digraph {
+        run -> intr
+        intr -> runbl
+        runbl -> run
+        run -> kernel
+        kernel -> zombie
+        kernel -> sleep
+        kernel -> runmem
+        sleep -> swap
+        swap -> runswap
+        runswap -> new
+        runswap -> runmem
+        new -> runmem
+        sleep -> runmem
+    }
+''', use_container_width=True)
---
+++ tree2.py
@@ -0,0 +1,5 @@
+class MemberNode:
+    def __init__(self, name):
+        self.name = name
+        self.left = None
+        self.right = None
