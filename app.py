f3299291c8223861e49b0219f0aac65eab623486
krishantt <154585072+krishantt@users.noreply.github.com>
2024-03-08 04:39:42 +0545

    fix:mlm tree

--- app.py
+++ app.py
@@ -2,34 +2,55 @@ import streamlit as st
 import networkx as nx
 import streamlit.components.v1 as components
 from pyvis.network import Network
+from utils.mlmtree import BinaryMLMTree, Node
 from options import options as graph_options
 
 
-def genarate_graph():
-    nt = Network(notebook=True, heading='', cdn_resources='remote')
-    nt.from_DOT("tree.DOT")
-    # nt.from_nx(nx.complete_graph(5))
+def generate_graph(tree):
+    nt = Network("800px", "1000px", notebook=True,
+                 heading='', cdn_resources='remote')
+    wr = tree.generate_DOT()
+    with open('tmp.DOT', 'w') as dotFile:
+        dotFile.write(wr)
+    nt.from_DOT("tmp.DOT")
     nt.toggle_physics(True)
-    nt.set_options(graph_options)
-    # nt.show_buttons()
-    nt.show('test.html')
+    nt.show('tmp.html')
+    with open('tmp.html', 'r', encoding='utf-8') as htmlFile:
+        source_code = htmlFile.read()
+        components.html(source_code, width=1000, height=800)
 
 
-def insert():
-    pass
+def main():
+    def insert_root(user):
+        st.session_state.tree.insert_root(user)
+
+    def insert(parent, side, user):
+        st.session_state.tree.insert(user, parent, side)
+
+    if "tree" not in st.session_state:  # make it run once
+        st.session_state.tree = BinaryMLMTree()
+
+    #program begins here 
+    st.title("Binary MLM Simulator")
+    placeholder = st.empty()
+    if st.session_state.tree.root is None:
+        with placeholder.container():
+            col1, col2 = st.columns([2, 1])
+            user = col1.text_input('User',
+                                   placeholder="Insert User..")
+            col2.button('Insert', on_click=lambda: insert_root(user))
+    else:
+        with placeholder.container():
+            col1, col2, col3, col4 = st.columns([2, 1, 1, 1])
+            parent = col1.selectbox('Where would you like the node to be added?',
+                                    st.session_state.tree.get_nodes(), index=None, placeholder="Select parent node..")
+            user = col2.text_input('User',
+                                   placeholder="Insert User..")
+            side = col3.selectbox('Side',
+                                  ('Left', 'Right', 'Spill'), index=None, placeholder="Insert Value..")
+            col4.button('Insert', on_click=lambda: insert(user, parent, side))
+    generate_graph(st.session_state.tree)
 
 
-st.title("Binary MLM Simulator")
-col1, col2, col3, col4 = st.columns([2, 1, 1, 1])
-col1.selectbox('Where would you like the node to be added?',
-               (1, 2, 3), index=None, placeholder="Select parent node..")
-col2.text_input('User',
-                placeholder="Insert User..")
-col3.selectbox('Side',
-               ('Left', 'Right'), index=None, placeholder="Insert Value..")
-col4.button('Insert', on_click=insert)
-
-genarate_graph()
-with open('test.html', 'r', encoding='utf-8') as htmlFile:
-    source_code = htmlFile.read()
-    components.html(source_code, width=1000, height=30000)
+if __name__ == '__main__':
+    main()
---
+++ utils/mlmtree.py
@@ -0,0 +1,123 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+    def __str__(self) -> str:
+        return self.val
+
+
+class BinaryMLMTree:
+    def __init__(self):
+        """
+        Initializes an empty binary tree.
+        """
+        self.root = None
+
+    def insert_root(self, val):
+        self.root = Node(val)
+
+    def insert(self, val, parent_key, side):
+        node = self.search(parent_key)
+        if node is None:
+            print("Parent key not found.")
+        else:
+            if side == 'Left':
+                if node.left is not None:
+                    print("items exist")
+                node.left = Node(val)
+            elif side == 'Right':
+                if node.right is not None:
+                    print("items exist")
+                node.right = Node(val)
+            else:
+                print("Invalid side.")
+
+    def search(self, key):
+        def dfs_search(node, key):
+            if node is None or node.val == key:
+                return node
+            # left search
+            left_result = dfs_search(node.left, key)
+            if left_result:
+                return left_result
+            # right search
+            right_result = dfs_search(node.left, key)
+            if right_result:
+                return right_result
+
+            return None
+
+        return dfs_search(self.root, key)
+
+    def get_nodes(self):
+        def get_nodes_r(node):
+            if node is None:
+                return
+            res.append(node)
+            get_nodes_r(node.left)
+            get_nodes_r(node.right)
+        res = []
+        get_nodes_r(self.root)
+        return res
+
+    def display_t(self, node):
+        def display_h(node):
+            if node is None:
+                return
+            if node.left is not None:
+                print(f"{node.val} -> {node.left.val}")
+            if node.right is not None:
+                print(f"{node.val} -> {node.right.val}")
+            display_h(node.left)
+            display_h(node.right)
+        display_h(self.root)
+
+    def generate_DOT(self):
+        def generate_DOT_r(node):
+            nonlocal res
+            if node is None:
+                return
+            if node.left is not None:
+                res += f"{node.val} -> {node.left.val};"
+            if node.right is not None:
+                res += f"{node.val} -> {node.left.val};"
+            if node.left is None and node.right is None:
+                res += f"{node.val};"
+        res = 'diagraph mlm {\n'
+        generate_DOT_r(self.root)
+        res += '\n}'
+        return res
+
+
+if __name__ == '__main__':
+    tree = BinaryMLMTree()
+
+    while True:
+        print("\n1. Insert")
+        print("2. Search")
+        print("3. Display")
+        print("4. Exit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            tree.insert(val, parent_key, side)
+        elif choice == '2':
+            key = int(input("Enter key to search: "))
+            if tree.search(tree.root, key):
+                print("Key found.")
+            else:
+                print("Key not found.")
+        elif choice == '3':
+            print("Tree structure: ")
+            tree.display_t(tree.root)
+            print()
+        elif choice == '4':
+            break
+        else:
+            print("Invalid choice.")
+
+
+#
