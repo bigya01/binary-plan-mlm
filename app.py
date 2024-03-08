5a25f3369e55c62d03914f2694a77d6fe22a59d3
krishantt <154585072+krishantt@users.noreply.github.com>
2024-03-08 18:01:21 +0545

    feat:added commission in ui

--- app.py
+++ app.py
@@ -2,9 +2,20 @@ import streamlit as st
 import networkx as nx
 import streamlit.components.v1 as components
 from pyvis.network import Network
+import pandas as pd
+
 from utils.mlmtree import BinaryMLMTree, Node
 from options import options as graph_options
+import utils.settings as s
 
+def generate_table(tree):
+    dictionary = tree.get_nodes_dict()
+    data = pd.DataFrame.from_dict(dictionary)
+    data['From Members'] = data['Members Added']*s.SIGNUP_COMMISION
+    data['From Sales'] = data['Sales']*s.PRODUCT_SOLD_COMMISSION
+    data['From Branched Members'] = data['Total Commission'] - data['From Members'] - data['From Sales']
+    # print(dictionary)
+    st.table(data)
 
 def generate_graph(tree):
     nt = Network("800px", "1000px", notebook=True,
@@ -22,13 +33,16 @@ def generate_graph(tree):
 
 
 def main():
-    #use callback for state management (streamlit hacks)
+    # use callback for state management (streamlit hacks)
     def insert_root(user):
         st.session_state.tree.insert_root(user)
 
     def insert(user, parent):
         # print(user,parent,side)
         st.session_state.tree.insert(user, parent)
+
+    def update_sales(node, key):
+        st.session_state.tree.update_sales(node, key)
 
     if "tree" not in st.session_state:  # make it run once
         st.session_state.tree = BinaryMLMTree()
@@ -46,10 +60,19 @@ def main():
         with placeholder.container():
             col1, col2, col3 = st.columns([2, 2, 1])
             parent = col1.selectbox('Where would you like the node to be added?',
-                                    st.session_state.tree.get_nodes(), index=None, placeholder="Select parent node..")
+                                    st.session_state.tree.get_nodes(), index=None, placeholder="Select parent node..", key=1)
             user = col2.text_input('User',
                                    placeholder="Insert User..")
-            col3.button('Insert', on_click=lambda: insert(user, str(parent)))
+            col3.button('Add User', on_click=lambda: insert(user, str(parent)))
+            generate_table(st.session_state.tree)
+            col1, col2, col3 = st.columns([2, 2, 1])
+            parent = col1.selectbox('Where would you like the node to be added?',
+                                    st.session_state.tree.get_nodes(), index=None, placeholder="Select parent node..", key=2)
+            sale = col2.number_input('Sales',
+                                     placeholder="Insert Sales..")
+            col3.button('Update', on_click=lambda: update_sales(
+                str(parent), int(sale)))
+
     generate_graph(st.session_state.tree)
 
 
--- utils/mlmtree.py
+++ utils/mlmtree.py
@@ -1,14 +1,18 @@
-import settings as s
+import utils.settings as s
+# import settings as s
+
+
 class Node:
     def __init__(self, val):
         self.val = val
         self.left = None
         self.right = None
-        self.sales=0
-        self.initial_com=0
-        self.total_com=0
-        self.added_members=0
-
+        self.sales = 0
+        self.initial_com = 0
+        self.total_com = 0
+        self.added_members = 0
+        self.color = 'blue'
+        self.is_Balanced = False
 
     def __str__(self) -> str:
         return self.val
@@ -23,16 +27,23 @@ class BinaryMLMTree:
 
     def insert_root(self, val):
         self.root = Node(val)
+        self.root.color = 'Red'
 
-    def update_commission(self,node):
+    def update_commission(self, node):
+        node.initial_com = 0
         if node.left and node.right:
-            node.initial_com+=s.BALANCE_COMMISSION
-        node.initial_com+=node.sales*s.PRODUCT_SOLD_COMMISSION
+            node.is_Balanced=True
+            node.initial_com += s.BALANCE_COMMISSION
+        node.initial_com += (node.sales*s.PRODUCT_SOLD_COMMISSION +
+                             node.added_members*s.SIGNUP_COMMISION)
 
     def insert(self, val, parent_key):
         node = self.search(self.root, parent_key)
-        node.added_members+=1
-        node.initial_com+=s.SIGNUP_COMMISION
+        if not node:
+            print("Not found")
+        node.added_members += 1
+        node.initial_com += s.SIGNUP_COMMISION
+
         def insert_node_with_spillover(node, val):
             if not node.left:
                 node.left = Node(val)
@@ -44,42 +55,46 @@ class BinaryMLMTree:
 
             elif node.left and node.right:
                 # node.intial_com+=BALANCE_COMMISSION
-                #recursion for spillover
+                # recursion for spillover
                 insert_node_with_spillover(node.left, val)
         insert_node_with_spillover(node, val)
 
-    def update_sales(self,node_key,sales):
+    def update_sales(self, node_key, sales):
         #  node_key=int(input("enter the node of which you want to add commision of"))
-        node=self.search(self.root,node_key)
+        node = self.search(self.root, node_key)
         #  sales=int(input("enter the sales of the node"))
-        node.sales+=sales
+        node.sales = sales
         self.update_commission(node)
+        # self.total_commision(node)
 
-    def total_sales(self,node):
-        if  node.left is None and node.right is None:
+    def total_sales(self, node):
+        if node is None:
+            return 0
+        if node.left is None and node.right is None:
             return node.sales
         else:
-            return node.sales +self.total_sales(node.left)+ self.total_sales(node.right)
-            
-    def total_commision(self,node):
-         if node.left is None and node.right is None:
+            return node.sales + self.total_sales(node.left) + self.total_sales(node.right)
+
+    def total_commision(self, node):
+        node.total_com = node.initial_com
+        if node.left is None and node.right is None:
             return node.initial_com
-         else:    
-            if self.total_sales(node.left)>self.total_sales(node.right):
-                node.total_com+=node.initial_com+ s.SALES_VOLULME_PER*self.total_sales(node.right)
+        else:
+            if self.total_sales(node.left) > self.total_sales(node.right):
+                node.total_com += s.SALES_VOLULME_PER*self.total_sales(node.right)
             else:
-                node.total_com+=node.initial_com+ s.SALES_VOLULME_PER*self.total_sales(node.left)
+                node.total_com += s.SALES_VOLULME_PER*self.total_sales(node.left)
             return node.total_com
 
     def search(self, node, key):
         if node is None or node.val == key:
             return node
-        left_result=self.search(node.left,key)
+        left_result = self.search(node.left, key)
         if left_result is not None:
-             return left_result
-        right_result=self.search(node.left,key)
+            return left_result
+        right_result = self.search(node.right, key)
         if right_result is not None:
-             return right_result
+            return right_result
         return None
 
     def get_nodes(self):
@@ -87,6 +102,23 @@ class BinaryMLMTree:
             if node is None:
                 return
             res.append(node)
+            get_nodes_r(node.left)
+            get_nodes_r(node.right)
+        res = []
+        get_nodes_r(self.root)
+        return res
+
+    def get_nodes_dict(self):
+        def get_nodes_r(node):
+            if node is None:
+                return
+            tmp = dict()
+            tmp['Name'] = node.val
+            tmp['Sales'] = node.sales
+            tmp['Members Added'] = node.added_members
+            self.total_commision(node)
+            tmp['Total Commission'] = node.total_com
+            res.append(tmp)
             get_nodes_r(node.left)
             get_nodes_r(node.right)
         res = []
@@ -110,6 +142,8 @@ class BinaryMLMTree:
             nonlocal res
             if node is None:
                 return
+            if node is not None:
+                res += f"{node.val} [color= \"{node.color}\", label=\"{node.val} | ${self.total_commision(node)}\"];\n"
             if node.left is not None:
                 res += f"{node.val} -> {node.left.val};\n"
             if node.right is not None:
@@ -119,8 +153,8 @@ class BinaryMLMTree:
             generate_DOT_r(node.left)
             generate_DOT_r(node.right)
         res = 'digraph mlm {\n'
-        if self.root:
-            res +=  f"{self.root.val} [color= \"Red\"];\n"
+        # if self.root:
+        #     res +=  f"{self.root.val} [color= \"Red\"];\n"
         generate_DOT_r(self.root)
         res += '}'
         return res
@@ -129,7 +163,7 @@ class BinaryMLMTree:
 if __name__ == '__main__':
     tree = BinaryMLMTree()
     tree.insert_root('bindu')
-    tree.insert('krishant','bindu')
+    tree.insert('krishant', 'bindu')
     tree.insert('krish', 'bindu')
     tree.insert('k2', 'krishant')
     tree.insert('k3', 'krish')
