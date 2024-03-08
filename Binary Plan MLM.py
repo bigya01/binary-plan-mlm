ab08eeb3e8678846436e765e6fe657b72f3ca022
krishantt <154585072+krishantt@users.noreply.github.com>
2024-03-08 23:06:52 +0545

    feat:added ui for avl tree

--- Binary Plan MLM.py
+++ Binary Plan MLM.py
@@ -1,29 +1,10 @@
 import streamlit as st
-import streamlit.components.v1 as components
-from pyvis.network import Network
-import pandas as pd
+
 
 from utils.mlmtree import BinaryMLMTree
+from utils.utils import generate_graph, generate_table
 
 
-def generate_table(tree):
-    dictionary = tree.get_nodes_dict()
-    data = pd.DataFrame.from_dict(dictionary)
-    data.reset_index(drop=True, inplace=True)
-    return data
-
-
-def generate_graph(tree):
-    nt = Network("800px", "1000px", notebook=True,
-                 heading='', cdn_resources='remote')
-    wr = tree.generate_DOT()
-    with open('tmp.DOT', 'w') as dotFile:
-        dotFile.write(wr)
-    nt.from_DOT("tmp.DOT")
-    nt.show('tmp.html')
-    with open('tmp.html', 'r', encoding='utf-8') as htmlFile:
-        source_code = htmlFile.read()
-        components.html(source_code, width=1000, height=800)
 
 
 def main():
--- pages/AVL Tree.py
+++ pages/AVL Tree.py
@@ -1,3 +1,34 @@
 import streamlit as st
+from utils.utils import generate_graph
+from utils.avltree import AVLTree
+
+
+def insert(value):
+    st.session_state.avltree
+
+def delete(value):
+    st.session_state.avltree
 
 st.title("AVL Tree Visualization")
+
+if "avltree" not in st.session_state:
+    st.session_state.avltree = AVLTree()
+
+placeholder = st.empty()
+col1, col2 = st.columns(2)
+
+with col1.container(border=True):
+    col11, col12 = st.columns(2)
+    insert_value = col11.number_input(
+        "Insert Node", placeholder="Insert Value", step=1)
+    col12.markdown('### ')
+    col12.button('Insert')
+
+with col2.container(border=True):
+    col21, col22 = st.columns(2)
+    insert_value = col21.number_input(
+        "Delete Node", placeholder="Insert Value", step=1)
+    col22.markdown('### ')
+    col22.button('Delete', type="primary")
+
+generate_graph(st.session_state.avltree)
